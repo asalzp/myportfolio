114642e783ae9e68a52d725adb2421970c447a4d
asalzp <azarepakziab@mun.ca>
2024-05-31 18:54:25 -0230

    removed full page lib

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Project from './pages/Projects';
-import Entertainment from './pages/Entertainment';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Projects";
+import Entertainment from "./pages/Entertainment";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-       <Routes>
-          <Route path="/*" element={<Home />} />
-          
-          
-       </Routes>
+      <nav className="navbar navbar-expand-lg custom-navbar fixed-top">
+        <a className="navbar-brand" href="#"></a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse navbar-items" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/projects">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/entertainment">
+                Entertainment
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/entertainment" element={<Entertainment />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
-
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,119 +1,35 @@
-import React, { useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
 import Intro from './Intro';
 import About from './About';
 import Projects from './Projects';
 import Entertainment from './Entertainment';
 import Contact from './Contact';
-import FullPage, { FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage';
 
 function Home() {
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const routes = ['/', '/about', '/projects', '/entertainment', '/contact'];
-    const sections = document.querySelectorAll('section');
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const sections = document.querySelectorAll('section');
-            const navLinks = document.querySelectorAll('.nav-item');
-            const routes = ['/', '/about', '/projects', '/entertainment', '/contact'];
-
-            sections.forEach((section, index) => {
-                const sectionTop = section.offsetTop;
-                const sectionHeight = section.clientHeight;
-
-                if (window.scrollY >= sectionTop - sectionHeight / 3 && window.scrollY < sectionTop + sectionHeight - sectionHeight / 3) {
-                    navLinks.forEach(link => link.classList.remove('active'));
-                    navLinks[index].classList.add('active');
-                    navigate(routes[index], { replace: true });
-                }
-            });
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [navigate]);
-
-    const scrollToSection = (id) => {
-        console.log(id)
-        const section = sections[id];
-        console.log(section)
-        if (section) {
-            window.scrollTo({
-                top: section.offsetTop,
-                behavior: 'smooth'
-            });
-        }
-    };
-
-    const handleNavClick = (e, id) => {
-        e.preventDefault();
-        scrollToSection(id);
-
-        e.preventDefault();
-    console.log('Navigating to section:', id);
-
-    // Collapse the navbar after clicking a link
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-
-    if (navbarToggler && navbarCollapse.classList.contains('show')) {
-      navbarToggler.click();
-    }
-    };
-
-    
+   
     return (
-        <FullPage>
-            <nav className="navbar navbar-expand-lg custom-navbar fixed-top">
-                <a className="navbar-brand" href="#"></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse navbar-items" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={(e) => handleNavClick(e, 0)}>Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={(e) => handleNavClick(e, 1)}>About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={(e) => handleNavClick(e, 2)}>Projects</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={(e) => handleNavClick(e, 3)}>Entertainment</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" onClick={(e) => handleNavClick(e, 4)}>Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <FullpageNavigation anchors={['home', 'about', 'projects', 'entertainment', 'contact']} />
-            <FullPageSections>
-                <FullpageSection id="home">
-                    <Intro />
-                </FullpageSection>
-                <FullpageSection id="about">
-                    <About />
-                </FullpageSection>
-                <FullpageSection id="projects">
-                    <Projects />
-                </FullpageSection>
-                <FullpageSection id="entertainment">
-                    <Entertainment />
-                </FullpageSection>
-                <FullpageSection id="contact">
-                    <Contact />
-                </FullpageSection>
-            </FullPageSections>
-        </FullPage>
+        <div>
+            <div>
+                <Intro />
+            </div>
+
+            <div>
+                <About />
+            </div>
+
+            <div>
+                <Projects />
+            </div>
+
+            <div>
+                <Entertainment />
+            </div>
+
+            <div>
+                <Contact />
+            </div>
+        </div>
+        
     );
 }
 
