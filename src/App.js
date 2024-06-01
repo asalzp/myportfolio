dc24b3effdcbc43b07c3e8fdfa53f4463f8dfe15
asalzp <azarepakziab@mun.ca>
2024-06-01 21:03:29 -0230

    navbar menu state mobile view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Project from "./pages/Projects";
 import Entertainment from "./pages/Entertainment";
 import Contact from "./pages/Contact";
-import { Link } from "react-scroll";
 
 function App() {
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const navRef = useRef(null);
+
+  const handleNavLinkClick = () => {
+    setIsNavCollapsed(true); // Collapse the navbar
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg custom-navbar fixed-top">
@@ -20,33 +27,40 @@ function App() {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          onClick={() => setIsNavCollapsed(!isNavCollapsed)}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse navbar-items" id="navbarNav">
+        <div
+          className={`collapse navbar-collapse navbar-items ${
+            isNavCollapsed ? "collapse" : "show"
+          }`}
+          id="navbarNav"
+          ref={navRef}
+        >
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#home">
+              <a className="nav-link" href="#home" onClick={handleNavLinkClick}>
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#about">
+              <a className="nav-link" href="#about" onClick={handleNavLinkClick}>
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#projects">
+              <a className="nav-link" href="#projects" onClick={handleNavLinkClick}>
                 Projects
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#entertainment">
+              <a className="nav-link" href="#entertainment" onClick={handleNavLinkClick}>
                 Entertainment
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#contact">
+              <a className="nav-link" href="#contact" onClick={handleNavLinkClick}>
                 Contact
               </a>
             </li>
